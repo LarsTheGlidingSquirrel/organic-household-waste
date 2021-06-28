946e9e2509ca35e466827c4c6e55d499bb677e87
Lars Rasmussen <lars@lars-rasmussen.de>
2021-06-28 13:03:24 +0200

    First page and start of biomüll.

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+// document.getElementById("star").onmouseover = () => {
+
+// 	gsap.to("#star", {duration: 0.1, scale: 1.1, ease: "power4.out"});
+// }
+
+// document.getElementById("star").onmouseleave = () => {
+
+// 	gsap.to("#star", {duration: 0.1, scale: 1, ease: "power4.out"});
+// }
+
+// document.getElementById("star").onmouseup = () => {
+
+// 	gsap.to("#star", {duration: 0.6, rotation: "+=360", ease: "power4.out"});
+// }
